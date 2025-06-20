4a72602fd97903bf67b246d2c0fe1c00f3bc1056
Mauricioagreda <50705877+Mauricioagreda@users.noreply.github.com>
2025-06-20 11:24:06 -0300

    Update-View
    View-Home-Index
    (comilla doble de mas)

--- obj/Debug/net8.0/Musica-MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/Musica-MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Musica-MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+846f6d7e781040f26109bd3cfad0668d21dc211a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Musica-MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Musica-MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
